5ef08d6800f51c79b07bf5ae1a94b41d86c7ca1e
simonboudreault <simonboudreault04@gmail.com>
2019-05-14 13:56:05 -0400

    updated server url

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL: 'http://localhost:5000'
+    // baseURL: 'http://localhost:5000'
+    baseURL: 'https://mongodb-crud-server.herokuapp.com/'
   })
 }
